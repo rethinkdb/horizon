181e4d2b1363bbbf9072d50e2a393d8a285c4211
Josh Kuhn <josh@rethinkdb.com>
2015-11-02 16:47:17 -0800

    Implementing new protocol changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import EventEmitter from "events"
 
+const PROTOCOL_VERSION = 'rethinkdb-fusion-v0'
+
 export class Fusion {
 
     constructor(hostString){
@@ -32,35 +34,33 @@ export class Fusion {
         }
     }
 
-    _query(path){
-        return this.socket.send("QUERY", path)
+    _query(query){
+        return this.socket.send("query", query)
     }
 
-    _store(path, document, conflict){
+    _store(collection, document, conflict){
         if(conflict === 'replace'){
-            return this.socket.send("STORE_REPLACE", {path: path, data: document})
+            return this.socket.send("store_replace", {collection: collection, data: document})
         }else if(conflict === 'error'){
-            return this.socket.send("STORE_ERROR", {path: path, data: document})
+            return this.socket.send("store_error", {collection: collection, data: document})
+        }else if(conflict === 'update'){
+            return this.socket.send("store_update", {collectionquery, data: document})
         }else{
             return Promise.reject(`value of conflict not understood: ${conflict}`)
         }
     }
 
-    _update(path, document){
-        return this.socket.send("UPDATE", {path: path, data: document})
+    _remove(collection, document){
+        return this.socket.send("remove", {collection: collection, data: document})
     }
 
-    _remove(path, id){
-        return this.socket.send("REMOVE", {path: path, data: id})
-    }
-
-    _subscribe(path, updates){
+    _subscribe(query, updates){
         if(updates){
-            return this.socket.subscribe(path)
+            return this.socket.subscribe(query)
         }else{
             //Emulate subscription with one-shot query
             var emitter = new EventEmitter()
-            this.socket.send("QUERY", path).then(
+            this.socket.send("query", query).then(
                 // Do we have continues etc like cursors?
                 // Right now assuming we get results back as an array
                 (results) => {
@@ -77,26 +77,34 @@ export class Fusion {
 
 class Socket {
     constructor(hostString, classifier){
-        this.ws = new WebSocket(`ws://${hostString}`)
-        this.ws.onmessage = this._onMessage
-        this.ws.onclose = this._onClose
-        this.ws.onopen = this._onOpen
+        this.ws = new WebSocket("ws://"+hostString, [PROTOCOL_VERSION])
+        // send handshake
         this.classifier = classifier
         this.promises = {}
         this.emitters = {}
         this.requestCounter = 0
+        this.performHandshake()
+    }
+
+    _performHandshake(){
+        this.ws.onmessage = () => {
+            this.ws.onmessage = this._onMessage
+            this.ws.onclose = this._onClose
+            this.ws.onopen = this._onOpen
+        }
+        this.ws.send("{}")
     }
 
     send(type, data){
         var requestId = this.requestCounter++
-        var req = {type: type, data: data, requestId: request_id}
+        var req = {type: type, options: data, requestId: request_id}
         return new Promise((resolve, reject) =>
             this.promises[requestId] = {resolve: resolve, reject:reject})
     }
 
-    subscribe(path){
+    subscribe(query){
         var requestId = this.requestCounter++
-        var req = {type: "SUBSCRIBE", data: data, request_id: requestId}
+        var req = {type: "subscribe", options: data, request_id: requestId}
         var emitter = new EventEmitter() // customize?
         this.emitters[requestId] = emitter
         return emitter
@@ -148,12 +156,12 @@ class TermBase {
 
     subscribe(updates: true){
         // should create a changefeed query, return eventemitter
-        return this.fusion._subscribe(this.path)
+        return this.fusion._subscribe(this.query)
     }
 
     value(){
         // return promise with no changefeed
-        return this.fusion._value(this.path)
+        return this.fusion._value(this.query)
     }
 }
 
@@ -161,100 +169,114 @@ class Collection {
 
     constructor(fusion, collectionName){
         this.collectionName = collectionName
-        this.path = {collection: collectionName}
+        this.query = {collection: collectionName}
         this.fusion = fusion
     }
 
     findOne(id){
-        return new FindOne(this.fusion, this.path, id)
+        return new FindOne(this.fusion, this.query, id)
     }
 
     find(fieldName, fieldValue){
-        return new Find(this.fusion, this.path, fieldName, fieldValue)
+        return new Find(this.fusion, this.query, fieldName, fieldValue)
     }
 
     between(minVal, maxVal, field: 'id'){
-        return new Between(this.fusion, this.path, minVal, maxVal,field)
+        return new Between(this.fusion, this.query, minVal, maxVal,field)
     }
 
-    order(field: 'id', descending: false){
-        return new Order(this.fusion, this.path, field, descending)
+    order(field: 'id', ascending: true){
+        return new Order(this.fusion, this.query, field, ascending)
     }
 
     store(document, conflict: 'replace'){
-        return this.fusion._store(this.path, document, conflict)
+        return this.fusion._store(this.query, document, conflict)
     }
 
     update(document){
-        return this.fusion._update(this.path, document)
+        return this.fusion._store(this.query, document, 'update')
     }
 
     remove(document){
-        return this.fusion._remove(this.path, document)
+        if(typeof document === 'number' || typeof document === 'string'){
+            document = {id: document}
+        }
+        return this.fusion._remove(this.query, document)
     }
 }
 
 class Find extends TermBase {
-    constructor(fusion, path, field, value){
+    constructor(fusion, query, field, value){
         super(fusion)
         this.field
         this.value = value
-        this.path = Object.assign({find: {field: field, value: value}}, path)
+        this.query = Object.assign({
+            selection: {type: 'find', args: [value]},
+            field_name: field
+        }, query)
     }
 
-    order(field: 'id', descending: false){
-        return new Order(this.fusion, this.path, field, descending)
+    order(field: 'id', ascending: true){
+        return new Order(this.fusion, this.query, field, ascending)
     }
 
     limit(size){
-        return new Limit(this.fusion, this.path, size);
+        return new Limit(this.fusion, this.query, size);
     }
 }
 
 class FindOne extends TermBase {
-    constructor(fusion, path, docId){
+    constructor(fusion, query, docId){
         super(fusion)
         this.id = docId
-        this.path = Object.assign({findOne: docId}, path)
+        this.query = Object.assign({
+            selection: {type: "findOne", args: [docId]}
+        }, query)
     }
 }
 
 class Between extends TermBase {
-    constructor(fusion, path, minVal, maxVal, field){
+    constructor(fusion, query, minVal, maxVal, field){
         super(fusion)
         this.minVal = minVal
         this.maxVal = maxVal
         this.field = field
-        this.path = Object.assign({between: {min: minVal, max: maxVal, field: field}}, path)
+        this.query = Object.assign({
+            selection: {type: "between", args: [minVal, maxVal]},
+            field_name: field
+        }, query)
     }
 
     limit(size){
-        return new Limit(this.fusion, this.path, size);
+        return new Limit(this.fusion, this.query, size);
     }
 }
 
 class Order {
-    constructor(fusion, path, field, descending){
+    constructor(fusion, query, field, ascending){
         this.fusion = fusion
         this.field = field
         this.direction = direction
-        this.path = Object.assign({order: {field: field, descending: descending}}, path)
+        this.query = Object.assign({
+            order: ascending ? "ascending" : "descending",
+            field_name: field
+        }, query)
     }
 
     limit(size){
-        return new Limit(this.fusion, this.path, size)
+        return new Limit(this.fusion, this.query, size)
     }
 
     between(minVal, maxVal){
         // between is forced to have the same field as this term
-        return new Between(this.fusion, this.path, minVal, maxVal, this.field)
+        return new Between(this.fusion, this.query, minVal, maxVal, this.field)
     }
 }
 
 class Limit extends TermBase {
-    constructor(fusion, path, size){
+    constructor(fusion, query, size){
         super(fusion)
         this.size = size
-        this.path = Object.assign({limit: size}, path)
+        this.query = Object.assign({limit: size}, query)
     }
 }
