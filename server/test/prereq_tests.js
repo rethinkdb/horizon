2f73b93cda8d64df3b0d3cc2cf9751abca1e7adf
Marc Hesse <marc.hesse@gmail.com>
2015-11-06 12:22:55 -0800

    adding missing file

---
+++ server/test/prereq_tests.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const utils = require('./utils.js');
+
+module.exports.name = 'Prereqs';
+
+module.exports.prepare_database = (done) => done();
+
+module.exports.all_tests = () => { };
