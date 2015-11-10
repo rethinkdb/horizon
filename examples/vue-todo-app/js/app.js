c231a71ef6d80b3b8698db6468932bf834582abe
Daniel Alan Miller <dalanmiller@rethinkdb.com>
2015-11-09 17:51:31 -0800

    Working todo-app example, changefeeds mostly working except for delete

--- examples/vue-todo-app/js/app.js
+++ examples/vue-todo-app/js/app.js
@@ -20,6 +20,7 @@
 		}
 	};
 
+	Vue.config.debug = true
 	exports.app = new Vue({
 
 		// the root element that will be compiled
@@ -27,7 +28,7 @@
 
 		// app initial state
 		data: {
-			todos: todoStorage.fetchAll(),
+			todos: [],
 			newTodo: '',
 			editedTodo: null,
 			visibility: 'all'
@@ -37,7 +38,7 @@
 		watch: {
 			todos: {
 				deep: true,
-				handler: todoStorage.saveOne
+				handler: todoStorage.saveAll
 			}
 		},
 
@@ -59,23 +60,32 @@
 						todo.completed = value;
 					});
 				}
-			}
+			},
+			ids: function() {
+				return this.todos.map(function(item){ return item.id })
+			},
 		},
 
 		// methods that implement data logic.
 		// note there's no DOM manipulation here at all.
 		methods: {
 
+			uuid: function() {
+  			var x = Math.floor(Math.random() * 100000000000);
+  			return Math.floor(Math.random() * x).toString(36) +
+        	Math.abs(Math.floor(Math.random() * x) ^ Date.now()).toString(36);
+			},
+
 			addTodo: function () {
 				var value = this.newTodo && this.newTodo.trim();
 				if (!value) {
 					return;
 				}
-				this.todos.push({ title: value, completed: false });
+				this.todos.unshift({ title: value, id: this.uuid(), completed: false });
 				this.newTodo = '';
 			},
 
-			removeTodo: function (todo) {
+			removeTodo: function (todo){
 				this.todos.$remove(todo);
 			},
 
@@ -102,7 +112,38 @@
 
 			removeCompleted: function () {
 				this.todos = filters.active(this.todos);
+			},
+
+			addedChanges: function (doc) {
+				console.log(doc);
+				console.log(this.ids.indexOf(doc.id));
+				if(this.ids.indexOf(doc.id) === -1){
+					this.todos.unshift(doc);
+				}
+			},
+
+			updatedChanges: function (doc) {
+				console.log("UPDATING");
+				console.log(doc);
+				todos.forEach(function (doc, index) {
+					if (todo.id == doc.new_val.id) {
+						todos[index] = doc.new_val;
+						return;
+					}
+				});
+			},
+
+			deletedChanges: function (doc) {
+				console.log("DELETIIION")
+				console.log(doc);
+				this.todos.forEach(function (doc, index) {
+					if (todo.id == doc.old_val.id) {
+						this.todos = this.todos.splice(index, 1);
+						return;
+					}
+				});
 			}
+
 		},
 
 		// a custom directive to wait for the DOM to be updated
@@ -120,5 +161,8 @@
 			}
 		}
 	});
+
+	todoStorage.fetchAll(app);
+	todoStorage.changes(app.addedChanges, app.updatedChanges, app.deletedChanges);
 
 })(window);
--- examples/vue-todo-app/js/store.js
+++ examples/vue-todo-app/js/store.js
@@ -1,33 +1,63 @@
 /*jshint unused:false */
-(function (exports) {
+(function(exports) {
 
-	'use strict';
+  'use strict';
 
-	var f = require("Fusion");
-	var Fusion = new f.Fusion("localhost:31420");
-	var todos = Fusion("todos");
-	var STORAGE_KEY = 'todos-vuejs';
+  const Fusion = require("Fusion");
+  const fusion = new Fusion("localhost:8181", {
+    secure: true
+  });
+  const todos = fusion("todos");
+  var STORAGE_KEY = 'todos-vuejs';
 
-	exports.todoStorage = {
+  exports.todoStorage = {
 
-		fetchAll: function() {
-			return todos.value();
-		},
-		saveOne: function (todo) {
-			//localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-			todos.update(todo);
-		},
-		saveAll: function(todos){
-			for(todo in todos){
-				this.saveOne(todo);
+    todos: todos,
+
+    fetchAll: function(app) {
+      console.log("FETCHING ALL")
+      todos.value().then(function(result) {
+
+        //Because value() on a collection is currently broken
+        result = result.map((item) => {
+          return item[0];
+        });
+
+        app.todos = result;
+
+      }).catch(function(error) {
+        console.error(error);
+      });
+    },
+    saveAll: function(newVal, oldVal) {
+			console.log("SAVEALL")
+
+			console.log(newVal.length);
+			console.log(oldVal.length);
+
+			const newIds = newVal.map(function(doc){return doc.id;});
+			console.log(newIds);
+
+			// Check if any of the todos in the old list are not in the computed
+			//  list of ids and call .remove
+			for (var doc of oldVal){
+				console.log(doc);
+				if (newIds.indexOf(doc.id) === -1) {
+					todos.remove(doc);
+				} else {
+					todos.store(doc, "replace");
+				}
 			}
+    },
+		remove: function(doc){
+			todos.remove(doc);
 		},
-		changes: function(added, updated, deleted){
-			todos
-				.on("added", added)
-				.on("updated", updated)
-				.on("deleted", deleted)
-		}
-	};
+    changes: function(added, updated, deleted) {
+      todos.subscribe()
+        .on("added", added)
+        .on("updated", updated)
+        .on("deleted", deleted)
+    }
+  };
 
 })(window);
