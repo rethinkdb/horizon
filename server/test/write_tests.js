4d6ac0c7f7e44d82224a6df34333a64994f41042
Marc Hesse <marc.hesse@gmail.com>
2015-11-12 14:19:17 -0800

    simplifying write tests

--- server/test/write_tests.js
+++ server/test/write_tests.js
@@ -3,439 +3,251 @@
 const utils = require('./utils');
 
 const assert = require('assert');
+const crypto = require('crypto');
+const r = require('rethinkdb');
 
-// Before each test, ids [0, 10) will be present in the table
-const num_rows = 10;
+// Before each test, ids [0, 4) will be present in the table
+const num_rows = 4;
 
-const new_row = { id: 10 };
-const conflict_row = { id: 0 };
-const new_batch = [ { id: 10 }, { id: 11 }, { id: 12 } ];
-const conflict_batch = [ { id: 8 }, { id: 9 }, { id: 10 } ];
+const new_id = [ 4 ];
+const conflict_id = [ 3 ];
+const new_ids = [ 4, 5, 6 ];
+const conflict_ids = [ 2, 3, 4 ];
 
 // TODO: verify through reql that rows have been inserted/removed
 const all_tests = (table) => {
+  const make_request = (type, ids) => {
+    return {
+      request_id: crypto.randomBytes(4).readUInt32BE(),
+        type: type,
+        options: {
+          collection: table,
+          data: ids.map((id) => ({ id, new_field: 'a' })),
+        },
+      };
+  }
+
   const check_table_size = (expected, done) => {
-    done();
+    r.table(table).count().run(utils.rdb_conn()).then((res) => {
+      assert.strictEqual(res, expected);
+      done();
+    }).catch((err) => done(err));
   };
 
   beforeEach('Clear table', (done) => utils.clear_table(table, done));
   beforeEach('Populate table', (done) => utils.populate_table(table, num_rows, done));
   beforeEach('Authenticate', (done) => utils.fusion_default_auth(done));
 
-  it('store new', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'store',
-        options: {
-          collection: table,
-          data: [ new_row ],
-        },
-      },
-      (err, res) => {
+  describe('Store', () => {
+    it('new', (done) => {
+      utils.stream_test(make_request('store', new_id), (err, res) => {
         assert.ifError(err);
-        assert.deepStrictEqual(res, [ 10 ]);
-        check_table_size(11, done);
+        assert.deepStrictEqual(res, new_id);
+        check_table_size(5, done);
       });
+    });
+
+    it('conflict', (done) => {
+      utils.stream_test(make_request('store', conflict_id), (err, res) => {
+        assert.ifError(err);
+        assert.deepStrictEqual(res, conflict_id);
+        check_table_size(4, done);
+      });
+    });
+
+    it('batch new', (done) => {
+      utils.stream_test(make_request('store', new_ids), (err, res) => {
+        assert.ifError(err);
+        assert.deepStrictEqual(res, new_ids);
+        check_table_size(7, done);
+      });
+    });
+
+    it('batch conflict', (done) => {
+      utils.stream_test(make_request('store', conflict_ids), (err, res) => {
+        assert.ifError(err);
+        assert.deepStrictEqual(res, conflict_ids);
+        check_table_size(5, done);
+      });
+    });
   });
 
-  it('store conflict', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'store',
-        options: {
-          collection: table,
-          data: [ conflict_row ],
-        },
-      },
-      (err, res) => {
-        assert.ifError(err);
-        assert.deepStrictEqual(res, [ 0 ]);
-        check_table_size(10, done);
-      });
-  });
-
-  it('store batch new', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'store',
-        options: {
-          collection: table,
-          data: new_batch,
-        },
-      },
-      (err, res) => {
-        assert.ifError(err);
-        assert.deepStrictEqual(res, [ 10, 11, 12 ]);
-        check_table_size(13, done);
-      });
-  });
-
-  it('store batch conflict', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'store',
-        options: {
-          collection: table,
-          data: conflict_batch,
-        },
-      },
-      (err, res) => {
-        assert.ifError(err);
-        assert.deepStrictEqual(res, [ 8, 9, 10 ]);
-        check_table_size(11, done);
-      });
-  });
-
-  it('replace new', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'replace',
-        options: {
-          collection: table,
-          data: [ new_row ],
-        },
-      },
-      (err, res) => {
+  describe('Replace', () => {
+    it('new', (done) => {
+      utils.stream_test(make_request('replace', new_id), (err, res) => {
         assert.notStrictEqual(err, null);
-        assert.strictEqual(err.message, `The document with id 10 was missing.`);
+        assert.strictEqual(err.message, `The document with id 4 was missing.`);
         assert.deepStrictEqual(res, [ ]);
-        check_table_size(10, done);
+        check_table_size(4, done);
       });
-  });
+    });
 
-  it('replace conflict', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'replace',
-        options: {
-          collection: table,
-          data: [ conflict_row ],
-        },
-      },
-      (err, res) => {
+    it('conflict', (done) => {
+      utils.stream_test(make_request('replace', conflict_id), (err, res) => {
         assert.ifError(err);
-        assert.deepStrictEqual(res, [ 0 ]);
-        check_table_size(10, done);
+        assert.deepStrictEqual(res, conflict_id);
+        check_table_size(4, done);
       });
-  });
+    });
 
-  it('replace batch new', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'replace',
-        options: {
-          collection: table,
-          data: new_batch,
-        },
-      },
-      (err, res) => {
+    it('batch new', (done) => {
+      utils.stream_test(make_request('replace', new_ids), (err, res) => {
         assert.notStrictEqual(err, null);
-        assert.strictEqual(err.message, `The document with id 10 was missing.`);
+        assert.strictEqual(err.message, `The document with id 4 was missing.`);
         assert.deepStrictEqual(res, [ ]);
-        check_table_size(10, done);
+        check_table_size(4, done);
       });
-  });
+    });
 
-  it('replace batch conflict', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'replace',
-        options: {
-          collection: table,
-          data: conflict_batch,
-        },
-      },
-      (err, res) => {
+    it('batch conflict', (done) => {
+      utils.stream_test(make_request('replace', conflict_ids), (err, res) => {
         assert.notStrictEqual(err, null);
-        assert.strictEqual(err.message, `The document with id 10 was missing.`);
+        assert.strictEqual(err.message, `The document with id 4 was missing.`);
         assert.deepStrictEqual(res, [ ]);
-        check_table_size(10, done);
+        check_table_size(4, done);
       });
+    });
   });
 
-  it('upsert new', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'upsert',
-        options: {
-          collection: table,
-          data: [ new_row ],
-        },
-      },
-      (err, res) => {
+  describe('Upsert', () => {
+    it('new', (done) => {
+      utils.stream_test(make_request('upsert', new_id), (err, res) => {
         assert.ifError(err);
-        assert.deepStrictEqual(res, [ 10 ]);
-        check_table_size(11, done);
+        assert.deepStrictEqual(res, new_id);
+        check_table_size(5, done);
       });
-  });
+    });
 
-  it('upsert conflict', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'upsert',
-        options: {
-          collection: table,
-          data: [ conflict_row ],
-        },
-      },
-      (err, res) => {
+    it('conflict', (done) => {
+      utils.stream_test(make_request('upsert', conflict_id), (err, res) => {
         assert.ifError(err);
-        assert.deepStrictEqual(res, [ 0 ]);
-        check_table_size(11, done);
+        assert.deepStrictEqual(res, conflict_id);
+        check_table_size(4, done);
       });
-  });
+    });
 
-  it('upsert batch new', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'upsert',
-        options: {
-          collection: table,
-          data: new_batch,
-        },
-      },
-      (err, res) => {
+    it('batch new', (done) => {
+      utils.stream_test(make_request('upsert', new_ids), (err, res) => {
         assert.ifError(err);
-        assert.deepStrictEqual(res, [ 10, 11, 12 ]);
-        check_table_size(11, done);
+        assert.deepStrictEqual(res, new_ids);
+        check_table_size(7, done);
       });
-  });
+    });
 
-  it('upsert batch conflict', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'upsert',
-        options: {
-          collection: table,
-          data: conflict_batch,
-        },
-      },
-      (err, res) => {
+    it('batch conflict', (done) => {
+      utils.stream_test(make_request('upsert', conflict_ids), (err, res) => {
         assert.ifError(err);
-        assert.deepStrictEqual(res, [ 8, 9, 10 ]);
-        check_table_size(11, done);
+        assert.deepStrictEqual(res, conflict_ids);
+        check_table_size(5, done);
       });
+    });
   });
 
-  it('update new', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'update',
-        options: {
-          collection: table,
-          data: [ new_row ],
-        },
-      },
-      (err, res) => {
+  describe('Update', () => {
+    it('new', (done) => {
+      utils.stream_test(make_request('update', new_id), (err, res) => {
         assert.notStrictEqual(err, null);
-        assert.strictEqual(err.message, `The document with id 10 was missing.`);
+        assert.strictEqual(err.message, `The document with id 4 was missing.`);
         assert.deepStrictEqual(res, [ ]);
-        check_table_size(10, done);
+        check_table_size(4, done);
       });
-  });
+    });
 
-  it('update conflict', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'update',
-        options: {
-          collection: table,
-          data: [ conflict_row ],
-        },
-      },
-      (err, res) => {
+    it('conflict', (done) => {
+      utils.stream_test(make_request('update', conflict_id), (err, res) => {
         assert.ifError(err);
-        assert.deepStrictEqual(res, [ 0 ]);
-        check_table_size(11, done);
+        assert.deepStrictEqual(res, conflict_id);
+        check_table_size(4, done);
       });
-  });
+    });
 
-  it('update batch new', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'update',
-        options: {
-          collection: table,
-          data: new_batch,
-        },
-      },
-      (err, res) => {
+    it('batch new', (done) => {
+      utils.stream_test(make_request('update', new_ids), (err, res) => {
         assert.notStrictEqual(err, null);
-        assert.strictEqual(err.message, `The document with id 10 was missing.`);
+        assert.strictEqual(err.message, `The document with id 4 was missing.`);
         assert.deepStrictEqual(res, [ ]);
-        check_table_size(10, done);
+        check_table_size(4, done);
       });
-  });
+    });
 
-  it('update batch conflict', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'update',
-        options: {
-          collection: table,
-          data: conflict_batch,
-        },
-      },
-      (err, res) => {
+    it('batch conflict', (done) => {
+      utils.stream_test(make_request('update', conflict_ids), (err, res) => {
         assert.notStrictEqual(err, null);
-        assert.strictEqual(err.message, `The document with id 10 was missing.`);
+        assert.strictEqual(err.message, `The document with id 4 was missing.`);
         assert.deepStrictEqual(res, [ ]);
-        check_table_size(11, done);
+        check_table_size(4, done);
       });
+    });
   });
 
-  it('insert new', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'insert',
-        options: {
-          collection: table,
-          data: [ new_row ],
-        },
-      },
-      (err, res) => {
+  describe('Insert', () => {
+    it('new', (done) => {
+      utils.stream_test(make_request('insert', new_id), (err, res) => {
         assert.ifError(err);
-        assert.deepStrictEqual(res, [ 10 ]);
-        check_table_size(11, done);
+        assert.deepStrictEqual(res, new_id);
+        check_table_size(5, done);
       });
-  });
+    });
 
-  it('insert conflict', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'insert',
-        options: {
-          collection: table,
-          data: [ conflict_row ],
-        },
-      },
-      (err, res) => {
+    it('conflict', (done) => {
+      utils.stream_test(make_request('insert', conflict_id), (err, res) => {
         assert.notStrictEqual(err, null);
         utils.check_error(err, 'Duplicate primary key');
         assert.deepStrictEqual(res, [ ]);
-        check_table_size(10, done);
+        check_table_size(4, done);
       });
-  });
+    });
 
-  it('insert batch new', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'insert',
-        options: {
-          collection: table,
-          data: new_batch,
-        },
-      },
-      (err, res) => {
+    it('batch new', (done) => {
+      utils.stream_test(make_request('insert', new_ids), (err, res) => {
         assert.ifError(err);
-        assert.deepStrictEqual(res, [ 10, 11, 12 ]);
-        check_table_size(11, done);
+        assert.deepStrictEqual(res, new_ids);
+        check_table_size(7, done);
       });
-  });
+    });
 
-  it('insert batch conflict', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'insert',
-        options: {
-          collection: table,
-          data: conflict_batch,
-        },
-      },
-      (err, res) => {
+    it('batch conflict', (done) => {
+      utils.stream_test(make_request('insert', conflict_ids), (err, res) => {
         assert.notStrictEqual(err, null);
         utils.check_error(err, 'Duplicate primary key');
         assert.deepStrictEqual(res, [ ]);
-        check_table_size(11, done);
+        check_table_size(5, done);
       });
+    });
   });
 
-  it('remove new', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'remove',
-        options: {
-          collection: table,
-          data: [ new_row ],
-        },
-      },
-      (err, res) => {
+  describe('Remove', () => {
+    it('new', (done) => {
+      utils.stream_test(make_request('remove', new_id), (err, res) => {
         assert.ifError(err);
-        assert.deepStrictEqual(res, [ 10 ]);
-        check_table_size(10, done);
+        assert.deepStrictEqual(res, new_id);
+        check_table_size(4, done);
       });
-  });
+    });
 
-  it('remove conflict', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'remove',
-        options: {
-          collection: table,
-          data: [ conflict_row ],
-        },
-      },
-      (err, res) => {
+    it('conflict', (done) => {
+      utils.stream_test(make_request('remove', conflict_id), (err, res) => {
         assert.ifError(err);
-        assert.deepStrictEqual(res, [ 0 ]);
-        check_table_size(9, done);
+        assert.deepStrictEqual(res, conflict_id);
+        check_table_size(3, done);
       });
-  });
+    });
 
-  it('remove batch new', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'remove',
-        options: {
-          collection: table,
-          data: new_batch,
-        },
-      },
-      (err, res) => {
+    it('batch new', (done) => {
+      utils.stream_test(make_request('remove', new_ids), (err, res) => {
         assert.ifError(err);
-        assert.deepStrictEqual(res, [ 10, 11, 12 ]);
-        check_table_size(10, done);
+        assert.deepStrictEqual(res, new_ids);
+        check_table_size(4, done);
       });
-  });
+    });
 
-  it('remove batch conflict', (done) => {
-    utils.stream_test(
-      {
-        request_id: 0,
-        type: 'remove',
-        options: {
-          collection: table,
-          data: conflict_batch,
-        },
-      },
-      (err, res) => {
+    it('batch conflict', (done) => {
+      utils.stream_test(make_request('remove', conflict_ids), (err, res) => {
         assert.ifError(err);
-        assert.deepStrictEqual(res, [ 8, 9, 10 ]);
-        check_table_size(8, done);
+        assert.deepStrictEqual(res, conflict_ids);
+        check_table_size(2, done);
       });
+    });
   });
 };
 
