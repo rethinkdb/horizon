13ad4ab16015318ceb4b16c80e51c1dc15e3a3cb
Daniel Alan Miller <dalanmiller@rethinkdb.com>
2015-11-16 18:02:34 -0800

    Fully working chat example

--- examples/vue-chat-app/app.js
+++ examples/vue-chat-app/app.js
@@ -1,14 +1,30 @@
+const Fusion = require("Fusion");
+const fusion = new Fusion("localhost:8181");
+const chat = fusion("chat");
+const app = new Vue({
 
-
-new Vue({
   el: '#app',
   data: {
     newMessage: "",
-    id: 1,
-    messages: [{text: 'Hello Vue.js!', id: 1}],
+    avatar_url: `http://api.adorable.io/avatars/50/${new Date().getMilliseconds()}.png`,
+    messages: [],
   },
 
   computed: {
+    limitedMessages: function(){
+      this.messages.sort(function(a,b){
+        if (a.datetime > b.datetime){
+          return 1;
+        } else if (a.datetime == b.datetime){
+          return 0;
+        } else {
+          return -1
+        }
+      });
+
+      return this.messages.slice(Math.max(this.messages.length - 6, 1))
+    }
+
 
   },
 
@@ -16,15 +32,31 @@ new Vue({
     addMessage: function(){
       var text = this.newMessage.trim();
       if (text){
-        this.id++;
-        this.messages.push({
+        chat.store({
           text: text,
-          id: this.id
+          id: this.avatar_url,
+          datetime: new Date().toString(),
         });
         this.newMessage = "";
       }
+    },
+
+    addedChange: function(newDoc){
+      //Parse string as proper Date Object
+      newDoc.datetime = new Date(Date.parse(newDoc.datetime));
+      this.messages.push(newDoc);
+    },
+
+    fetchAll: function(){
+      chat.value().then((result) => {
+        result.forEach((doc) => {
+          doc.datetime = new Date(Date.parse(doc.datetime));
+        })
+
+        this.messages = this.messages.concat(result);
+      });
     }
   }
-
-
-})
+});
+app.fetchAll();
+chat.subscribe().on("added", app.addedChange);
