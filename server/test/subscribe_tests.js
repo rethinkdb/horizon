00ed43aa2b068b043639580e8e99c7b2f8e5dcc1
Marc Hesse <marc.hesse@gmail.com>
2016-01-11 15:52:15 -0800

    adding type tags to subscribe requests

--- server/test/subscribe_tests.js
+++ server/test/subscribe_tests.js
@@ -1,6 +1,14 @@
 'use strict';
 
-const all_tests = () => { };
+const utils = require('./utils');
+
+const all_tests = (table) => {
+  const num_rows = 10;
+
+  before('Clear table', (done) => utils.clear_table(table, done));
+  before('Populate table', (done) => utils.populate_table(table, num_rows, done));
+  beforeEach('Authenticate client', utils.fusion_default_auth);
+};
 
 const suite = (table) => describe('Subscribe', () => all_tests(table));
 
