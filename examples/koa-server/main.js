b25a8d0befe797bc3985b0f19e74cc4daf0c584a
Marc Hesse <marc@rethinkdb.com>
2016-01-13 15:59:51 -0800

    adding stubs for server integrations

---
+++ examples/koa-server/main.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node --harmony-destructuring
+'use strict'
+
+const koa = require('koa');
+const fusion = require('fusion-server');
+
+const app = koa();
+const http_server = app.listen(8181);
+const fusion_server = fusion(http_server);
+
+console.log('Listening on port 8181.');
