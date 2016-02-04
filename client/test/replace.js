1e708107405d05724413a3848ed3b84719bfefcd
Josh Kuhn <josh@rethinkdb.com>
2016-02-03 19:52:29 -0800

    Replace tests pass

--- client/test/replace.js
+++ client/test/replace.js
@@ -1,131 +1,111 @@
-replaceSuite = (getData) => {
-  return () => {
-
-  var data;
+'use strict'
+const replaceSuite = getData => () => {
+  let data
 
   before(() => {
-    data = getData();
-  });
+    data = getData()
+  })
+
+  // Let's store a document first, then replace it.
+  it('replaces an existing document completely', done => {
+    data.store({ id: 1, a: { b: 1, c: 1 }, d: 1 }).toArray()
+      // should return an array with an ID of the inserted document.
+      .do(res => assert.deepEqual(res, [ 1 ]))
+      // Let's make sure we get back the document that we put in.
+      .flatMap(() => data.find(1).fetch())
+      // Check that we get back what we put in.
+      .do(res => assert.deepEqual(res, { id: 1, a: { b: 1, c: 1 }, d: 1 }))
+      // Let's replace the document now
+      .flatMap(() => data.replace({ id: 1, a: { c: 2 } })).toArray()
+      // We should have gotten the ID back again
+      .do(res => assert.deepEqual(res, [ 1 ]))
+      // Make sure `replace` replaced the original document
+      .flatMap(() => data.find(1).fetch())
+      // Check that the document was updated correctly
+      .do(res => assert.deepEqual(res, { id: 1, a: { c: 2 } }))
+      .subscribe(doneObserver(done))
+  })
 
   // The `replace` command replaces documents already in the database. It
   // errors if the document doesn't exist.
-  it("#.replace(single_non_existent)", (done) => {
-    data.replace({ id: 1, a: 1, b: 1 }).catch((err) => {
-      assert.isDefined(err);
-      assert.isNotNull(err);
-      done();
-    });
-  });
+  it('fails if the document does not already exist', done => {
+    data.replace({ id: 1, a: 1, b: 1 }).subscribe(doneErrorObserver(done))
+  })
 
   // It means you can't replace a document without providing an id.
-  it("#.replace(single_non_id)", (done) => {
-    data.replace({ a: 1, b: 1 }).catch((err) => {
-      assert.isDefined(err);
-      assert.isNotNull(err);
-      done();
-    });
-  });
-
-  // Let's store a document first, then replace it.
-  it("#.replace(single_existing)", (done) => {
-    data.store({ id: 1, a: { b: 1, c: 1 }, d: 1 }).then((res) => {
-      // The promise should return an array with an ID of the inserted
-      // document.
-      assert.deepEqual([1], res);
-
-      // Let's make sure we get back the document that we put in.
-      return data.find(1).value();
-    }).then((res) => {
-      // Check that we get back what we put in.
-      assert.deepEqual({ id: 1, a: { b: 1, c: 1 }, d: 1 }, res);
-
-      // Let's replace the document now
-      return data.replace({ id: 1, a: { c: 2 } });
-    }).then((res) => {
-      // We should have gotten the ID back again
-      assert.deepEqual([1], res);
-
-      // Make sure `replace` replaced the original document
-      return data.find(1).value();
-    }).then((res) => {
-      // Check that the document was updated correctly
-      assert.deepEqual({ id: 1, a: { c: 2 } }, res);
-      done();
-    }).catch(done);
-  });
+  it('fails if document does not have an id', done => {
+    data.replace({ a: 1, b: 1 }).subscribe(doneErrorObserver(done))
+  })
 
   // Calling `replace` with `null` is an error.
-  it("#.replace(null)", (done) => {
-    try {
-      data.replace(null);
-    } catch(err) { done(); }
-  });
+  it('fails if null is passed', assertThrows(
+    'The argument to replace must be non-null',
+    () => data.replace(null)
+  ))
 
   // Calling `replace` with `undefined` is also an error.
-  it("#.replace(undefined)", (done) => {
-    try {
-      data.replace();
-    } catch(err) { done(); }
-  });
+  it('fails if undefined is passed', assertThrows(
+    'The 1st argument to replace must be defined',
+    () => data.replace(undefined)
+  ))
+
+  it('fails if passed no arguments', assertThrows(
+    'replace must receive exactly 1 argument',
+    () => data.replace()
+  ))
 
   // The `replace` command allows storing multiple documents in one call.
   // Let's replace a few documents and make sure we get them back.
-  it("#.replace(multiple)", (done) => {
-    data.store([{ id: 1, a: { b: 1, c: 1 }, d: 1 },
-                { id: 2, a: { b: 2, c: 2 }, d: 2 }]).then((res) => {
-      // The promise should return an array with an ID of the inserted
-      // document.
-      assert.deepEqual([1, 2], res);
-
+  it('allows replacing multiple documents with one call', done => {
+    data.store([
+      { id: 1, a: { b: 1, c: 1 }, d: 1 },
+      { id: 2, a: { b: 2, c: 2 }, d: 2 },
+    ]).toArray()
+      // should return an array with an ID of the inserted document.
+      .do(res => assert.deepEqual(res, [ 1, 2 ]))
       // Let's make sure we get back the documents that we put in.
-      return data.findAll(1, 2).value();
-    }).then((res) => {
+      .flatMap(() => data.findAll(1, 2).fetch({ asCursor: false }))
       // Check that we get back what we put in.
-      assert.sameDeepMembers([{ id: 1, a: { b: 1, c: 1 }, d: 1 },
-                              { id: 2, a: { b: 2, c: 2 }, d: 2 }],
-                             res);
-
+      .do(res => assert.sameDeepMembers(res, [
+        { id: 1, a: { b: 1, c: 1 }, d: 1 },
+        { id: 2, a: { b: 2, c: 2 }, d: 2 },
+      ]))
       // All right. Let's update the documents now
-      return data.replace([{ id: 1, a: { c: 2 } },
-                           { id: 2, d: 3 }]);
-    }).then((res) => {
+      .flatMap(() => data.replace([
+        { id: 1, a: { c: 2 } },
+        { id: 2, d: 3 },
+      ]))
+      .toArray()
       // We should have gotten the ID back again
-      assert.deepEqual([1, 2], res);
-
+      .do(res => assert.deepEqual(res, [ 1, 2 ]))
       // Make sure `update` updated the documents properly
-      return data.findAll(1, 2).value();
-    }).then((res) => {
+      .flatMap(() => data.findAll(1, 2).fetch({ asCursor: false }))
       // Check that we get back what we put in.
-      assert.sameDeepMembers([{ id: 1, a: { c: 2 } },
-                              { id: 2, d: 3 }],
-                             res);
-
-      done();
-    }).catch(done);
-  });
+      .do(res => assert.sameDeepMembers(res, [
+        { id: 1, a: { c: 2 } },
+        { id: 2, d: 3 },
+      ]))
+      .subscribe(doneObserver(done))
+  })
 
   // If any operation in a batch update fails, everything is reported as a
   // failure. Note that we're updating `null` below, and a document with
   // no ID. Both are failures.
-  it("#.replace(multiple_one_null)", (done) => {
-    data.replace([{ id: 1, a: 1 }, null, { a: 1 }]).catch((err) => {
-      assert.isDefined(err);
-      assert.isNotNull(err);
-      done();
-    });
-  });
+  it('fails if any document in a batch fails to be replaced', done => {
+    data.replace([ { id: 1, a: 1 }, null, { a: 1 } ])
+      .subscribe(doneErrorObserver(done))
+  })
 
   // Replacing an empty batch of documents is ok, and returns an empty
   // array.
-  it("#.replace(empty_batch)", (done) => {
-    data.replace([]).then((res) => {
-      // The promise should return an array with the IDs of the documents
-      // in order, including the generated IDS.
-      assert.isArray(res);
-      assert.lengthOf(res, 0);
-      done();
-    }).catch(done);
-  });
-
-  } // Testing `replace`
-}
+  it('allows an empty batch of documents', done => {
+    data.replace([])
+      .do(res => {
+        // should return an array with the IDs of the documents in
+        // order, including the generated IDS.
+        assert.isArray(res)
+        assert.lengthOf(res, 0)
+      })
+      .subscribe(doneObserver(done))
+  })
+} // Testing `replace`
