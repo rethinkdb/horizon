c6ac9d73374d7428f819843f1196d73b35bc9484
Josh Kuhn <josh@rethinkdb.com>
2016-02-05 17:31:50 -0800

    Above tests pass

--- client/test/above.js
+++ client/test/above.js
@@ -1,159 +1,123 @@
-aboveSuite = (getData) => {
-  return () => {
-
-  var data;
+'use strict'
+const aboveSuite = (getData) => () => {
+  let data
 
   before(() => {
-    data = getData();
-  });
+    data = getData()
+  })
 
   // By default `above` is closed
-  it("#.order(id).above(5)", (done) => {
-    data.order('id').above({ id: 5 }).value().then((res) => {
-      assert.deepEqual([{ id: 5, a: 60 },
-                        { id: 6, a: 50 }], res);
-      done();
-    }).catch(done);
-  });
+  it('is a closed bound by default', assertCompletes(() =>
+    data.order('id').above({ id: 5 }).fetch({ asCursor: false })
+      .do(res => assert.deepEqual(res, [
+        { id: 5, a: 60 },
+        { id: 6, a: 50 },
+      ]))
+  ))
 
   // We can also pass that explicitly
-  it("#.order(id).above(5, closed)", (done) => {
-    data.order('id').above({ id: 5 }, 'closed').value().then((res) => {
-      assert.deepEqual([{ id: 5, a: 60 },
-                        { id: 6, a: 50 }], res);
-      done();
-    }).catch(done);
-  });
+  it('allows "closed" to be passed explicitly', assertCompletes(() =>
+    data.order('id').above({ id: 5 }, 'closed').fetch({ asCursor: false })
+      .do(res => assert.deepEqual(res, [
+        { id: 5, a: 60 },
+        { id: 6, a: 50 },
+      ]))
+  ))
 
   // But we can make it open
-  it("#.order(id).above(5, open)", (done) => {
-    data.order('id').above({ id: 5 }, 'open').value().then((res) => {
-      assert.deepEqual([{ id: 6, a: 50 }], res);
-      done();
-    }).catch(done);
-  });
+  it('can return an open bounded result', assertCompletes(() =>
+    data.order('id').above({ id: 5 }, 'open').fetch()
+      .do(res => assert.deepEqual(res, { id: 6, a: 50 }))
+  ))
 
   // Let's try something that returns no values
-  it("#.order(id).above(maxval)", (done) => {
-    data.order('id').above({ id: 7 }).value().then((res) => {
-      assert.deepEqual([], res);
-      done();
-    }).catch(done);
-  });
+  it('returns no results if bound eliminates all documents',
+     assertCompletes(() =>
+    data.order('id').above({ id: 7 }).fetch()
+      .do(() => assert.fail())
+  ))
 
   // We can chain `above` off a collection
-  it("#.above({id:5})", (done) => {
-    data.above({ id: 5 }).value().then((res) => {
-      assert.isArray(res);
-      assert.lengthOf(res, 2)
-      done();
-    }).catch(done);
-  });
+  it('can be chained from a collection directly', assertCompletes(() =>
+    data.above({ id: 5 }).fetch({ asCursor: false })
+      .do(res => {
+        assert.isArray(res)
+        assert.lengthOf(res, 2)
+      })
+  ))
 
   // Or off other things
-  it("#.findAll.above({id:5})", (done) => {
-    data.findAll({ a: 20 }).above({ id: 3 }).value().then((res) => {
-      assert.isArray(res);
-      assert.lengthOf(res, 2)
-      done();
-    }).catch(done);
-  });
+  it('can be chained from a findAll', assertCompletes(() =>
+    data.findAll({ a: 20 }).above({ id: 3 }).fetch({ asCursor: false })
+      .do(res => {
+        assert.isArray(res)
+        assert.lengthOf(res, 2)
+      })
+  ))
 
   // `above` can't include any keys that are in `findAll`
-  it("#.findAll(a).above(a)", (done) => {
-    data.findAll({ a: 20 }).above({ a: 3 }).value().catch((err) => {
-      assert.isDefined(err);
-      assert.isNotNull(err);
-      done();
-    });
-  });
+  it('errors when it contains any keys from the findAll term', assertErrors(() =>
+    data.findAll({ a: 20 }).above({ a: 3 }).fetch()
+  ))
 
   // Let's try it on a non-primary key
-  it("#.order([a,id]).above([20])", (done) => {
-    data.order(['a', 'id']).above({ a: 20 }).value().then((res) => {
-      assert.deepEqual([{ id: 2, a: 20, b: 1 },
-                        { id: 3, a: 20, b: 2 },
-                        { id: 4, a: 20, b: 3 },
-                        { id: 6, a: 50 },
-                        { id: 5, a: 60 }], res);
-      done();
-    }).catch(done);
-  });
+  it('can be used on a non-primary key', assertCompletes(() =>
+    data.order([ 'a', 'id' ]).above({ a: 20 }).fetch({ asCursor: false })
+      .do(res => assert.deepEqual(res, [
+        { id: 2, a: 20, b: 1 },
+        { id: 3, a: 20, b: 2 },
+        { id: 4, a: 20, b: 3 },
+        { id: 6, a: 50 },
+        { id: 5, a: 60 },
+      ]))
+  ))
 
   // Let's try it on a non-primary key, but open
-  it("#.order([a,id]).above([20], open)", (done) => {
-    data.order(['a', 'id']).above({ a: 20 }, 'open').value().then((res) => {
-      assert.deepEqual([{ id: 6, a: 50 },
-                        { id: 5, a: 60 }], res);
-      done();
-    }).catch(done);
-  });
-
+  it('can be used on non-primary key with open bound', assertCompletes(() =>
+    data.order([ 'a', 'id' ]).above({ a: 20 }, 'open').fetch({ asCursor: false })
+      .do(res => assert.deepEqual(res, [
+        { id: 6, a: 50 },
+        { id: 5, a: 60 },
+      ]))
+  ))
   // The key in `above` must be the first key in `order`
-  it("#.order([a,id]).above(id)", (done) => {
-    data.order(['a', 'id']).above({ id: 20 }).value().catch((err) => {
-      assert.isDefined(err);
-      assert.isNotNull(err);
-      done();
-    });
-  });
+  it('must receive as an argument the first key in the order term', assertErrors(() =>
+    data.order([ 'a', 'id' ]).above({ id: 20 }).fetch()
+  ))
 
   // Passing multiple keys to `above` isn't legal
-  it("#.order([a,id]).above(multiple)", (done) => {
-    data.order(['a', 'id']).above({ a: 20, id: 20 }).value().catch((err) => {
-      assert.isDefined(err);
-      assert.isNotNull(err);
-      done();
-    });
-  });
+  it('errors if multiple keys are passed', assertErrors(() =>
+    data.order([ 'a', 'id' ]).above({ a: 20, id: 20 }).fetch()
+  ))
 
   // Nor is passing a field that isn't specified in `order`
-  it("#.order([a,id]).above(other_field)", (done) => {
-    data.order(['a', 'id']).above({ b: 20 }).value().catch((err) => {
-      assert.isDefined(err);
-      assert.isNotNull(err);
-      done();
-    });
-  });
+  it(`errors if the field passed isn't in the order term`, assertErrors(() =>
+    data.order([ 'a', 'id' ]).above({ b: 20 }).fetch()
+  ))
 
   // If chaining `above/below`, they must be passed the same key
-  it("#.above(key1:5).below(key2: 6)", (done) => {
-    data.above({ b: 0 }).below({ a: 100 }).value().catch((err) => {
-      assert.isDefined(err);
-      assert.isNotNull(err);
-      done();
-    });
-  });
+  it(`errors if it doesn't receive the same key as the below term`,
+     assertErrors(() =>
+    data.above({ b: 0 }).below({ a: 100 }).fetch()
+  ))
 
   // Starting with `null` is not ok
-  it("#.above(null)", (done) => {
-    try {
-      data.above(null).value();
-    } catch(err) { done(); }
-  });
+  it('throws if it is passed null', assertThrows(
+    'The 1st argument to above must be non-null',
+    () => data.above(null).fetch()
+  ))
 
   // Empty value is not ok
-  it("#.above()", (done) => {
-    try {
-      data.above().value();
-    } catch(err) { done(); }
-  });
+  it('throws if it does not receive an argument', assertThrows(
+    'above must receive at least 1 argument.',
+    () => data.above().fetch()
+  ))
 
   // Bad arguments are not ok
-  it("#.above(1)", (done) => {
-    data.above(1).value().catch((err) => {
-      assert.isDefined(err);
-      assert.isNotNull(err);
-      done();
-    });
-  });
-  it("#.above(id:1, bad)", (done) => {
-    data.above({ id: 1 }, 1).value().catch((err) => {
-      assert.isDefined(err);
-      assert.isNotNull(err);
-      done();
-    });
-  });
-
-  } // Testing `above`
-}
+  it('errors if it receives a non-string argument', assertErrors(() =>
+    data.above(1).fetch()
+  ))
+  it('errors if it receives more than one argument', assertErrors(() =>
+    data.above({ id: 1 }, 1).fetch()
+  ))
+} // Testing `above`
