a96bf88f241ba09f7816e59371bba35f17bb9e10
Josh Kuhn <josh@rethinkdb.com>
2016-02-05 18:22:32 -0800

    Chaining tests pass

--- client/test/chaining.js
+++ client/test/chaining.js
@@ -1,74 +1,73 @@
-chainingSuite = (getData) => {
-  return () => {
-
-  var data;
+'use strict'
+const chainingSuite = (getData) => () => {
+  let data
 
   before(() => {
-    data = getData();
-  });
+    data = getData()
+  })
 
   // Let's do a biiig chain
-  it("#.findAll.order.above.below", (done) => {
+  it('findAll.order.above.below', assertCompletes(() =>
     data.findAll({ a: 20 })
-        .order('id').above({ id: 2 }).below({ id: 4 })
-        .value().then((res) => {
-      assert.deepEqual([{ id: 2, a: 20, b: 1 },
-                        { id: 3, a: 20, b: 2 }], res);
-      done();
-    }).catch(done);
-  });
+      .order('id')
+      .above({ id: 2 })
+      .below({ id: 4 })
+      .fetch({ asCursor: false })
+      .do(res => assert.deepEqual(res, [
+        { id: 2, a: 20, b: 1 },
+        { id: 3, a: 20, b: 2 }
+      ]))
+  ))
 
   // Let's flip it the other way and change the order
-  it("#.findAll.below.above.order(desc)", (done) => {
+  it('findAll.below.above.order(desc)', assertCompletes(() =>
     data.findAll({ a: 20 })
-        .below({ id: 4 }).above({ id: 2 })
-        .order('id', 'descending')
-        .value().then((res) => {
-      assert.deepEqual([{ id: 3, a: 20, b: 2 },
-                        { id: 2, a: 20, b: 1 }], res);
-      done();
-    }).catch(done);
-  });
+      .below({ id: 4 })
+      .above({ id: 2 })
+      .order('id', 'descending')
+      .fetch({ asCursor: false })
+      .do(res => assert.deepEqual(res, [
+        { id: 3, a: 20, b: 2 },
+        { id: 2, a: 20, b: 1 },
+      ]))
+  ))
 
   // Let's throw limit into the mix
-  it("#.findAll.order.above.below.limit", (done) => {
+  it('findAll.order.above.below.limit', assertCompletes(() =>
     data.findAll({ a: 20 })
-        .above({ id: 2 }).order('id').below({ id: 4 }).limit(1)
-        .value().then((res) => {
-      assert.deepEqual([{ id: 2, a: 20, b: 1 }], res);
-      done();
-    }).catch(done);
-  });
+      .above({ id: 2 })
+      .order('id').below({ id: 4 }).limit(1)
+      .fetch({ asCursor: false })
+      .do(res => assert.deepEqual(res, [{ id: 2, a: 20, b: 1 }]))
+  ))
 
   // Let's do it on the collection
-  it("#.order.above.below.limit", (done) => {
-    data.below({ id: 4 }).order('id').above({ id: 2 }).limit(1)
-        .value().then((res) => {
-      assert.deepEqual([{ id: 2, a: 20, b: 1 }], res);
-      done();
-    }).catch(done);
-  });
+  it('order.above.below.limit', assertCompletes(() =>
+    data.below({ id: 4 })
+      .order('id')
+      .above({ id: 2 })
+      .limit(1)
+      .fetch({ asCursor: false })
+      .do(res => assert.deepEqual(res, [{ id: 2, a: 20, b: 1 }]))
+  ))
 
   // Let's try a big compound example
-  it("#.findAll.order.above.below.limit", (done) => {
+  it('findAll.order.above.below.limit', assertCompletes(() =>
     data.findAll({ a: 20 })
-        .order('id')
-        .above({ id: 2 })
-        .below({ id: 4 }, 'closed')
-        .limit(2)
-        .value().then((res) => {
-      assert.deepEqual([{ id: 2, a: 20, b: 1 },
-                        { id: 3, a: 20, b: 2 }], res);
-      done();
-    }).catch(done);
-  });
+      .order('id')
+      .above({ id: 2 })
+      .below({ id: 4 }, 'closed')
+      .limit(2)
+      .fetch({ asCursor: false })
+      .do(res => assert.deepEqual(res, [
+        { id: 2, a: 20, b: 1 },
+        { id: 3, a: 20, b: 2 },
+      ]))
+  ))
 
   // Can't chain off vararg `findAll`
-  it("#.findAll(a, b).order", (done) => {
-    try {
-      data.findAll({ a: 20 }, { a: 50 }).order('id').value();
-    } catch(err) { done(); }
-  });
-
-  } // Testing more advanced chaining
-}
+  it(`throws if order is chained off a multi-arg findAll`, assertThrows(
+    'order cannot be called on the current query',
+    () => data.findAll({ a: 20 }, { a: 50 }).order('id').fetch()
+  ))
+} // Testing more advanced chaining
