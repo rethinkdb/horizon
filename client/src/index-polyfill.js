07e8f0000fbab20126011f5cfc0db15f35681b13
Petri Lievonen <pete@screen.io>
2016-03-04 14:40:10 +0200

    Polyfill only the needed features Ran tests in Win7/IE10 via BrowserStack and added polyfills until there were no errors. Replaces usage of `core-js/shim` with specific polyfills (Array.from, Array#findIndex, Array#keys, Object.assign). This reduces the gzipped size from 65 KB to 47 KB, most of which is still Rx. Also moved core-js from devDependencies to dependencies proper, for importing lib/ in module packagers. Also upgraded babel-runtime, babel-plugin-transform-runtime and core-js to "^2.1.0" so they use the same versions, allowing deduping by webpack optimizer. Also increased timeout of in-browser tests to 10 s. Also fixed recent babel change of .babelrc resolving, fixing build error (the project may need to start pinning the dependencies soon).

--- client/src/index-polyfill.js
+++ client/src/index-polyfill.js
@@ -1,9 +1,16 @@
-require('core-js/shim')
+// Ensures these features are present or polyfilled
+// See http://kangax.github.io/compat-table/es6/
+require('core-js/fn/array/from');
+require('core-js/fn/array/find-index');
+require('core-js/fn/array/keys');
+require('core-js/fn/object/assign');
 
 if (typeof window !== 'undefined') {
   if (typeof window.Rx !== 'undefined') {
     // Insert helpful warning here
   } else {
+    // In polyfill version we expose Rx, as users generally want to use the
+    // same Rx as the library itself -- for example `Rx.Observable.empty()`
     window.Rx = require('rx')
   }
 }
