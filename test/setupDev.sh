8bfed438494255b368c1b8561f68d027feb22629
Josh Kuhn <josh@rethinkdb.com>
2016-03-04 13:54:20 -0800

    Remove node_modules in setupDev.sh

--- test/setupDev.sh
+++ test/setupDev.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 set -e
+
+green () {
+    echo -e "\033[1;32m== $1 \033[0m"
+}
+
+green 'Removing old node_modules dirs if present'
+if [ -d ../client/node_modules ]; then
+  echo Removing client/node_modules
+  rm --recursive ../client/node_modules
+fi
+if [ -d ../server/node_modules ]; then
+  echo Removing server/node_modules
+  rm --recursive ../server/node_modules
+fi
+if [ -d ../cli/node_modules ]; then
+  echo Removing cli/node_modules
+  rm --recursive ../cli/node_modules
+fi
+
 pushd ../client
+green 'Unlinking existing client'
+npm unlink
+green 'Linking client'
 npm link
 popd
 pushd ../server
-npm link ../client
+green 'Unlinking existing server'
+npm unlink
+green 'Linking server'
+npm link '@horizon/client'
 npm link
 popd
 pushd ../cli
-npm link ../server
-npm link ../client
+green 'Unlinking existing horizon cli'
+npm unlink
+green 'Linking horizon cli'
+npm link '@horizon/client'
+npm link '@horizon/server'
 npm link
 popd
-echo 'Dev environment set up'
+green 'Dev environment set up'
