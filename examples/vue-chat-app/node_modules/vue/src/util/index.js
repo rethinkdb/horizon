6ecadca470fadfe6ae8b59fbf6ffc3e0d31127c5
Daniel Alan Miller <dalanmiller@rethinkdb.com>
2016-04-11 16:55:33 -0700

    Cleaning up examples dir and moving files into dist

--- examples/vue-chat-app/node_modules/vue/src/util/index.js
+++
@@ -1,9 +0,0 @@
-var lang = require('./lang')
-var extend = lang.extend
-
-extend(exports, lang)
-extend(exports, require('./env'))
-extend(exports, require('./dom'))
-extend(exports, require('./options'))
-extend(exports, require('./component'))
-extend(exports, require('./debug'))
--- examples/vue-todo-app/node_modules/vue/src/directives/internal/index.js
+++
@@ -1,5 +0,0 @@
-exports.style = require('./style')
-exports['class'] = require('./class')
-exports.component = require('./component')
-exports.prop = require('./prop')
-exports.transition = require('./transition')
