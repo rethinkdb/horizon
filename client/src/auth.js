728f965ea95f7b307c3e36672746ff9356f4e680
Josh Kuhn <josh@rethinkdb.com>
2016-04-18 20:11:28 -0700

    Move check for localStorage
    Fixes #263

--- client/src/auth.js
+++ client/src/auth.js
@@ -33,10 +33,10 @@ class FakeStorage {
 }
 
 function getStorage() {
-  if (window.localStorage === undefined) {
-    return new FakeStorage()
-  }
   try {
+    if (window.localStorage === undefined) {
+      return new FakeStorage()
+    }
     window.localStorage.setItem('$$fake', 1)
     window.localStorage.removeItem('$$fake')
     return window.localStorage
