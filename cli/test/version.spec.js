98b67b4d90ef2eeeca709e6565ad4b174a44da09
Jim Anders <jim@janders223.com>
2016-04-20 18:30:32 -0400

    WIP Add tests for init cli command

--- cli/test/version.spec.js
+++ cli/test/version.spec.js
@@ -9,11 +9,11 @@ describe('Version Command', () => {
   // Sadly this intercepts mocha output and you don't see
   // we either need to find something better to output information
   // in the terminal, or a better way to capture the output in test
-  beforeEach(() => { sinon.stub(console, 'log'); });
-  afterEach(() => { console.log.restore(); });
+  beforeEach(() => { sinon.stub(console, 'info'); });
+  afterEach(() => { console.info.restore(); });
 
   it('prints the version and exits', () => {
     versionCommand.runCommand();
-    assert.equal(console.log.args[0][0], pkg.version);
+    assert.equal(console.info.args[0][0], pkg.version);
   });
 });
