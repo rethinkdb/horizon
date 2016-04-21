e851fdaf7ce6356748b6d5fb7c1411a1ef19abbf
Josh Kuhn <deontologician@gmail.com>
2016-04-21 10:20:40 -0700

    Remove outdated comment

--- cli/test/version.spec.js
+++ cli/test/version.spec.js
@@ -6,9 +6,6 @@ const sinon = require('sinon');
 const pkg = require('../package.json');
 
 describe('Version Command', () => {
-  // Sadly this intercepts mocha output and you don't see
-  // we either need to find something better to output information
-  // in the terminal, or a better way to capture the output in test
   beforeEach(() => { sinon.stub(console, 'info'); });
   afterEach(() => { console.info.restore(); });
 
