da71382626ab6c3b795b1b8d8f1801248eb30731
Ryan Paul <ryan@rethinkdb.com>
2016-05-04 23:58:42 +0000

    Refactored model and stubbed out data browser

--- adminui/dist/index.js
+++ adminui/dist/index.js
@@ -1,46 +1,54 @@
-
 function timestamp() { return (new Date).getTime() / 1000; }
 
-const sets = {
+Object.entries = object =>
+  Object.keys(object)
+        .filter(key => object.hasOwnProperty(key))
+        .map(key => [key, object[key]]);
+
+const adminQueries = {
   "clients": ["clients", {}],
   "requests": ["requests", {}],
   "cursors": ["requests", {live: true}],
   "collections": ["collections", {}]
 };
 
-const state = {
-  navigation: {
-    selected: "Dashboard",
-    items: [
-      "Dashboard",
-      "Users",
-      "Collections",
-      "Requests",
-      "Servers"
-    ]
+const initialState = {
+  state: {
+    browser: {
+      maxrows: 50,
+      collection: null,
+    },
+    navigation: {
+      selected: "Dashboard",
+      items: [
+        "Dashboard",
+        "Users",
+        "Collections",
+        "Requests",
+        "Servers"
+      ]
+    }
+  },
+  data: {
+    browser: [],
+    dashboard: Object.assign({},
+                  ...Object.keys(adminQueries).map(k => ({[k]: []})))
   }
 };
 
-let entries = object =>
-  Object.keys(object)
-        .filter(key => object.hasOwnProperty(key))
-        .map(key => [key, object[key]]);
-        
 class Model {
   constructor() {
-    this.graph = null;
     this.horizon = Horizon();
+    this.freezer = new Freezer(initialState);
     
-    this.store = new Freezer(
-      Object.assign({state: state}, 
-        entries(sets).reduce((prev, [k,v]) =>
-          Object.assign({[k]: []}, prev), {})));
-    
-      
-    for (let [set, [command, opts]] of entries(sets))
+    for (let [set, [command, opts]] of Object.entries(adminQueries))
       this.horizon.send(`admin:${command}`, opts)
                   .forEach(item => this.update(set, item));
-
+    
+    this.store.state.browser.getListener()
+        .on("update", ch => this.onBrowserStateChange(ch));
+        
+        
     setInterval(() => this.updateGraph(), 1000);
 
     window.addEventListener("resize", ev => {
@@ -50,27 +58,34 @@ class Model {
     });
   }
   
-  navigate(place) {
-    this.data().state.navigation.set({selected: place});
-  }
-
-  data() {
-    return this.store.get();
-  }
-
   subscribe(fn) {
-    this.store.on("update", fn);
+    this.freezer.on("update", fn);
   }
+  
+  get store() {
+    return this.freezer.get();
+  }
+  
+  update(set, {old_val, new_val, type, state}) {
+    let data = this.store.data.dashboard[set];
 
+    if (type === "initial")
+      data.push(new_val);
+    else if (type === "add")
+      data.unshift(new_val);
+    else if (type === "remove")
+      data.splice(data.indexOf(old_val), 1);
+  }
+  
   updateGraph() {
-    if (this.graph) {
-      let data = this.data();
+    if (!this.graph) return;
+    
+    let {clients, cursors} = this.store.data.dashboard;
 
-      this.graph.push([
-        {time: timestamp(), y: data.clients.length},
-        {time: timestamp(), y: data.cursors.length}
-      ]);
-    }
+    this.graph.push([
+      {time: timestamp(), y: clients.length},
+      {time: timestamp(), y: cursors.length}
+    ]);
   }
 
   initializeGraph() {
@@ -83,17 +98,12 @@ class Model {
       ]
     });
   }
-
-  update(set, {old_val, new_val, type, state}) {
-    let data = this.data()[set];
-
-    //console.log(set, type, new_val);
-
-    if (type === "initial")
-      data.push(new_val);
-    else if (type === "add")
-      data.unshift(new_val);
-    else if (type === "remove")
-      data.splice(data.indexOf(old_val), 1);
+  
+  onBrowserStateChange({maxrows, collection}) {
+    if (this.browserWatch) this.browserWatch.dispose();
+    
+    this.browserWatch = Horizon()(collection)
+                        .order("id").limit(20).watch()
+                        .forEach(x => this.store.data.browser.reset(x));
   }
 }
