41625b2acb4cceb15c453323371ed730676caea7
Patrick Neschkudla <neschkudla@gmail.com>
2016-05-13 11:30:30 +0200

    require isomorphic-fetch with imports & exports loader to make it work everywhere

--- client/src/util/fetch.js
+++ client/src/util/fetch.js
@@ -1,7 +1,9 @@
 import { Observable } from 'rxjs/Observable'
 import { fromPromise } from 'rxjs/observable/fromPromise'
 import { map } from 'rxjs/operator/map'
-import fetch from 'isomorphic-fetch'
+
+global.self = global
+require('imports?this=>global!exports?global.fetch!isomorphic-fetch')
 
 export default function fetchJSON(url) {
   return Observable::fromPromise(fetch(url))
