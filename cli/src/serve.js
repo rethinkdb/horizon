36591bc6e17b531c46bf95d16a37ef4241697280
Daniel Alan Miller <dalan.miller@gmail.com>
2016-05-15 00:00:24 -0500

    Fixing arg help message

--- cli/src/serve.js
+++ cli/src/serve.js
@@ -42,11 +42,11 @@ const addArguments = (parser) => {
 
   parser.addArgument([ '--key-file' ],
     { type: 'string', metavar: 'PATH',
-      help: 'Path to the key file to use, defaults to "./key.pem".' });
+      help: 'Path to the key file to use, defaults to "./horizon-key.pem".' });
 
   parser.addArgument([ '--cert-file' ],
     { type: 'string', metavar: 'PATH',
-      help: 'Path to the cert file to use, defaults to "./cert.pem".' });
+      help: 'Path to the cert file to use, defaults to "./horizon-cert.pem".' });
 
   parser.addArgument([ '--token-secret' ],
     { type: 'string', metavar: 'SECRET',
