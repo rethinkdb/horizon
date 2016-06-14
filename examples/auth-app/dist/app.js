0ff3637637e95541feb99471f384d4c119227e41
Daniel Alan Miller <dalanmiller@rethinkdb.com>
2016-06-14 15:20:58 -0700

    Simple auth example ready

--- examples/auth-app/dist/app.js
+++ examples/auth-app/dist/app.js
@@ -1,15 +1,16 @@
 'use strict'
-
-const horizon = Horizon({ authType: 'anonymous'});
+var horizon = Horizon({
+    authType: 'anonymous'
+});
 
 const crossOut = (element) => {
-  element.setAttribute("style", "text-decoration: line-through; color: grey;");
+    element.setAttribute("style", "text-decoration: line-through; color: grey;");
 }
 
 // #1 - Connect to Horizon server
 
 horizon.onReady(() => {
-  crossOut(document.querySelector('#connection-success'));
+    crossOut(document.querySelector('#connection-success'));
 })
 
 horizon.connect();
@@ -17,18 +18,42 @@ horizon.connect();
 // #2 - Ensure Github OAuth client_id & client_secret added
 
 fetch("/horizon/auth_methods").then((response) => {
-  return response.text();
+    return response.text();
 }).then((json) => {
-  console.log(json)
-  const strategies = JSON.parse(json);
-  if (strategies.auth && strategies.indexOf('github') >= 0){
-    crossOut(document.querySelector('#github-configured'));
-  }
+    const strategies = JSON.parse(json);
+    if (strategies.hasOwnProperty('github')) {
+        console.log(strategies)
+        crossOut(document.querySelector('#github-configured'));
+        const login = document.querySelector('#login-button');
+        login.disabled = false;
+        login.className = login.className.replace('btn-error-outline', 'btn-primary-outline');
+    }
 })
 
+// #3 - Test OAuth pathing with Github
 
-// #3 - Create
+var horizon = Horizon({
+    authType: 'token'
+});
+horizon.connect()
 
-if(!horizon.hasAuthToken()){
-  
+if (!horizon.hasAuthToken()) {
+    console.log("no auth token")
+    document.querySelector('#login-button').addEventListener('click', () => {
+        console.log('clicked');
+        horizon.authEndpoint('github').subscribe((endpoint) => {
+            window.location.pathname = endpoint;
+        });
+    });
+} else {
+    const user = horizon.currentUser().fetch().forEach((user) => {
+        document.querySelector('#success').innerHTML = `
+    <div class="col-sm-12">
+    <div class="card card-inverse card-success">
+        <div class="card-block">
+            <h1 class="card-title">Authentication successful <br> Your user ID is ${user.id}</h1>
+        </div>
+    </div>
+    </div> `;
+    });
 }
