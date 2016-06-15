0688a3ec7bce19c63a0e17d6b06e109a1f20a917
Patrick Neschkudla <neschkudla@gmail.com>
2016-06-16 01:57:48 +0200

    remove isomorphic-fetch and imports/exports loaders (fixes #590, #571, #477, #420) (#593)
    * remove isomorphic-fetch and imports/exports loaders, use whatwg-fetch, replace webpack specific syntax in fetch.js and move it to webpack config
    
    * remove deprecated loaders
    
    * Revert "remove deprecated loaders"
    
    This reverts commit 90bc2d0a29c2e4ab5b9b3c8d16d8e5a0067ec248.

--- client/src/util/fetch.js
+++ client/src/util/fetch.js
@@ -2,9 +2,6 @@ import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/observable/fromPromise'
 import 'rxjs/add/operator/mergeMap'
 
-global.self = global
-require('imports?this=>global!exports?global.fetch!isomorphic-fetch')
-
 export default function fetchJSON(url) {
   return Observable.fromPromise(fetch(url))
     .mergeMap(response => {
