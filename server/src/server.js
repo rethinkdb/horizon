dcbd431dd36926f5f4802a40a475d485b8891fd2
ericb <eric@hellouser.net>
2016-07-06 09:52:38 -0500

    use correct option

--- server/src/server.js
+++ server/src/server.js
@@ -80,7 +80,7 @@ class Server {
                                          opts.auto_create_collection,
                                          opts.auto_create_index,
                                          opts.rdb_user || null,
-                                         opts.rdb_pass || null,
+                                         opts.rdb_password || null,
                                          opts.rdb_timeout || null);
     this._auth = new Auth(this, opts.auth);
     for (const key in endpoints) {
