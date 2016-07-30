8e20cfed73438587dcc4dd1caec1861fe5eaa40d
Josh Kuhn <josh@rethinkdb.com>
2016-07-29 19:29:49 -0700

    wip

--- client/test/observableUtils.js
+++ client/test/observableUtils.js
@@ -1,93 +1,5 @@
 import { assert } from 'chai'
 
-// A clone of some of the ava api to make testing observables easier
-class TestSpec {
-  constructor(done) {
-    this._done = done
-    this._expectedPasses = 0
-    this._expected = null
-    this._equalityFunc = null
-    this._obtained = []
-    this._numPassed = 0
-  }
-
-  plan(length) {
-    this._expectedPasses = length
-  }
-
-  pass() {
-    this._numPasses += 1
-  }
-
-  fail(msg = 'Error is failed') {
-    this._done(new Error(msg))
-  }
-
-  expect(expected, equalityFunc = assert.deepEqual) {
-    this._expected = expected
-    this._expectedPasses = expected.length
-    this._equalityFunc = equalityFunc
-  }
-
-  end() {
-    const expectedPasses = this._expectedPasses
-    const expected = this._expected
-    const passes = this._numPasses
-    const obtained = this._obtained
-
-    if (expected !== null) {
-      if (expectedPasses < obtained.length) {
-        this._done(new Error(
-          `Expected only ${expectedPasses} values but got ${obtained.length}`))
-        return
-      } else if (expectedPasses > obtained.length) {
-        this._done(new Error(
-          `Expected ${expectedPasses} values but only got ${obtained.length}`))
-        return
-      }
-      for (let i = 0; i < expectedPasses; i++) {
-        if (this._equalityFunc(obtained[i], expected[i])) {
-          this.pass()
-        }
-      }
-    }
-    if (expectedPasses < passes) {
-      this._done(new Error(
-        `Expected only ${expectedPasses} passes, but got ${passes}`))
-    } else if (expectedPasses > passes) {
-      this._done(new Error(
-        `Expected ${expectedPasses} passes but got only ${passes}`))
-    } else {
-      this._done()
-    }
-  }
-}
-
-export function observableTest(func) {
-  return done => {
-    const t = new TestSpec(done)
-    const result = func(t)
-    if (typeof result.subscribe === 'function') {
-      result.subscribe({
-        next(val) {
-          if (this._expected !== null) {
-            t._obtained.push(val)
-          }
-        },
-        error(e) {
-          done(e)
-        },
-        complete() {
-          t.end()
-        },
-      })
-      return undefined
-    } else {
-      return result
-    }
-  }
-}
-
 export function oit(title, implementation) {
   it(title, observableTest(implementation))
 }
@@ -99,33 +11,56 @@ export class ObservableTestPlan {
   }
 
   beforeHand(action) {
-    this._plan.push([ 'before', action ])
+    this._plan.push({
+      type: 'before',
+      action,
+    })
     return this
   }
 
   subscribeTo(query) {
-    this._plan.push([ 'subscribe' ])
+    this._plan.push({
+      type: 'subscribe',
+      action(observer) {
+        return query.subscribe(observer)
+      },
+    })
     this._query = query
     return this
   }
 
-  expect(value, equality = assert.deepEqual) {
-    this._plan.push([ 'expect', value, equality ])
+  expect(expected, equality = assert.deepEqual) {
+    this._plan.push({
+      type: 'expect',
+      test(val) {
+        return equality(val, expected)
+      },
+    })
     return this
   }
 
   execute(action) {
-    this._plan.push([ 'execute', action ])
+    this._plan.push({
+      type: 'execute',
+      action() {
+        action.subscribe()
+      },
+    })
     return this
   }
 
   expectComplete() {
-    this._plan.push([ 'complete' ])
+    this._plan.push({ type: 'complete' })
     return this
   }
 
   expectError(regex) {
-    this._plan.push([ 'error', regex ])
+    this._plan.push({
+      type: 'error',
+      test(e) {
+        return Boolean(e.message.match(regex))
+      },
+    })
     return this
   }
 }
@@ -145,31 +80,20 @@ class TestObserver {
     this._done = done
   }
 
-  // Check out the type of the next instruction
-  peekType() {
-    if (this.plan[0] !== undefined) {
-      return this.plan[0][0]
-    } else {
-      return undefined
-    }
-  }
-
   expect(val) {
-    const [ t, expected, compare ] = this.plan.shift()
-    compare(val, expected)
+    this.plan.shift().test(val)
   }
 
   execute() {
-    const [ t, action ] = this.plan.shift()
-    action.subscribe()
+    this.plan.shift().action.subscribe()
   }
 
   errorMatch(e) {
-    const [ t, regex ] = this.plan.shift()
-    if (e.message.match(regex)) {
+    const errTest = this.plan.shift()
+    if (errTest.test(e)) {
       this.done()
     } else {
-      this.done(`Expected error to match ${regex}. Got: "${e.message}"`)
+      this.done(`Expected error to match ${errTest.regex}. Got: "${e.message}"`)
     }
   }
 
@@ -179,7 +103,7 @@ class TestObserver {
   done(msg) {
     if (msg instanceof Error) {
       this._done(msg)
-    } else if (typeof 'msg' === 'string') {
+    } else if (typeof msg === 'string') {
       this._done(new Error(msg))
     } else if (this.plan.length === 0) {
       this._done(new Error(`Test plan wasn't empty. Next was: ${this.plan[0]}`))
@@ -191,10 +115,10 @@ class TestObserver {
   // Observer methods below
 
   next(val) {
-    let peek = this.peekType()
-    if (peek === 'expect') {
+    if (this.plan[0].type === 'expect') {
       this.expect(val)
     } else {
+
     }
     while (this.peekType('execute')) {
       this.execute()
@@ -219,4 +143,31 @@ class TestObserver {
       this.done(`Planned ${this.plan[0][0]} but completed instead`)
     }
   }
+}
+
+
+export function expectRunEncoder(plan) {
+  let skip = 0
+  let take = 0
+  const runs = []
+  let currentRun = { expects: [] }
+  for (const p of plan) {
+    if (p.type === 'expect') {
+      currentRun.expects.push(p)
+      take += 1
+    } else {
+      currentRun.skip = skip
+      currentRun.take = take
+      runs.push(currentRun)
+      currentRun = { expects: [] }
+      skip += take
+      take = 0
+    }
+  }
+  if (currentRun.expects.length > 0) {
+    currentRun.skip = skip
+    currentRun.take = take
+    runs.push(currentRun)
+  }
+  return runs
 }
