cbf069cc4391ed57f0af3438ed6c52b67977f74a
Josh Kuhn <josh@rethinkdb.com>
2016-08-16 18:03:28 -0700

    feature: proc-promise utility

---
+++ cli/src/utils/proc-promise.js
@@ -0,0 +1,23 @@
+const Promise = require('bluebird');
+const childProcess = require('child_process');
+
+function procPromise() {
+  // Takes the same arguments as child_process.spawn
+  const args = Array.prototype.slice.call(arguments);
+  return new Promise((resolve, reject) => {
+    const proc = childProcess.spawn.apply(childProcess, args);
+    proc.stderr.setEncoding('utf8');
+    proc.stdout.setEncoding('utf8');
+    proc.on('exit', (code) => {
+      if (code === 0) {
+        resolve(proc);
+      } else {
+        reject(new Error(proc.stderr.read()));
+      }
+    })
+  });
+}
+
+module.exports = {
+  procPromise
+}
