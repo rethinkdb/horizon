7eab3106a7c6f4abe1fec09f5e1be7d486085544
Marc Hesse <marc@rethinkdb.com>
2016-09-02 11:00:59 -0700

    fixed up setupDev.sh to provision plugins

--- setupDev.sh
+++ setupDev.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-do_clean=$([ "$1" == "--clean" ])
+if [ "$1" == "--clean" ]; then
+  do_clean=true
+fi
 
 green () {
     echo -e "\033[1;32m== $1 \033[0m"
@@ -14,20 +16,21 @@ link_dir () {
   shift
 
   pushd $dir
-  if [ do_clean = "true" ]; then
+  if [ "$do_clean" = "true" ]; then
     echo Removing $dir/node_modules
     rm -r node_modules
   fi
 
   green "Unlinking $dir"
   npm unlink
-  green "Linking $dir"
 
+  green "Linking $dir deps"
   while (( "$#" )); do
     npm link "$1"
     shift
   done
 
+  green "Linking $dir"
   npm link --unsafe-perm --cache-min 9999999
   popd
 }
@@ -35,16 +38,22 @@ link_dir () {
 link_dir client
 link_dir server "@horizon/client"
 link_dir cli "@horizon/server"
-link_dir plugins/utils "@horizon/server"
+link_dir plugin-router
 
-plugin_names=($(ls -1d plugins/* | grep -v -e utils -e defaults))
+# Link all the plugins - 'utils' must go first, and 'defaults' must go last
+pushd plugins
+link_dir utils "@horizon/server"
+
+plugin_names=($(ls -1d * | grep -v -e utils -e defaults))
 plugin_modules=()
 for plugin_name in "${plugin_names[@]}"; do
-  link_dir "plugins/$plugin_name" "@horizon/server" "@horizon/plugin-utils"
+  link_dir "$plugin_name" "@horizon/server" "@horizon/plugin-utils"
   plugin_modules+=("@horizon/plugin-$plugin_name")
 done
 
-link_dir plugins/defaults ${plugin_modules[@]}
-link_dir test "@horizon/plugin-defaults" "@horizon/plugin-router" "@horizon/server" "@horizon"
+link_dir defaults ${plugin_modules[@]}
+popd
+
+link_dir test "@horizon/plugin-defaults" "@horizon/plugin-router" "@horizon/server" "horizon"
 
 green "Dev environment ready"
