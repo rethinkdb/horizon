0a26b5093798499fb707948233863b5b3362a4a8
Michael Lucy <mlucy@rethinkdb.com>
2016-09-05 02:52:08 +0000

    stuffs

--- symlinkDev.sh
+++ symlinkDev.sh
@@ -4,13 +4,17 @@ for i in plugins/*; do
     {
         cd $i
         plugins=`cat package.json \
-                   | perl -n -e '/^ *"\@horizon\/plugin-([^"]*)\"/ && print "$1\n"' \
-                   | grep -v router`;
+                   | perl -n -e '/^ *"\@horizon-plugins\/([^"]*)\"/ && print "$1\n"'`;
         mkdir -p node_modules/@horizon
+        mkdir -p node_modules/@horizon-plugins
+
         cd node_modules/@horizon
         ln -f -n -s ../../../../server server
+        ln -f -n -s ../../../../plugin-utils plugin-utils
+
+        cd ../@horizon-plugins
         for p in $plugins; do
-            ln -f -n -s ../../../$p plugin-$p
+            ln -f -n -s ../../../$p $p
         done
     } &
 done
