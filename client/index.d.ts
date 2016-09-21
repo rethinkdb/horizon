a11ac62921d44ad951001c511a78b4324aef9bc0
Marshall Cottrell <marshall007@mac.com>
2016-09-20 19:38:03 -0500

    Typings Fixes (#835)
    * fix return types for `.fetch()` and `.watch()`, export additional types
    
    Fixes #834
    
    * add `.hasAuthToken()` and `.authEndpoint()`

--- client/index.d.ts
+++ client/index.d.ts
@@ -2,17 +2,21 @@
 import { Observable } from 'rxjs';
 
 declare namespace hz {
+
+    interface Feed {
+        watch (options?: { rawChanges: boolean }): Observable<any>;
+        fetch (): Observable<any>;
+    }
+
     type Bound = 'open' | 'closed';
     type Direction = 'ascending' | 'descending';
     type Primitive = boolean | number | string | Date;
     type IdValue = Primitive | Primitive[] | { id: Primitive };
+    type WriteOp = Object | Object[];
 
-    interface TermBase {
-        watch (options?: { rawChanges: boolean }): TermBase;
-        fetch (): TermBase;
-
-        findAll (...values: IdValue[]): TermBase;
+    interface TermBase extends Feed {
         find (value: IdValue): TermBase;
+        findAll (...values: IdValue[]): TermBase;
 
         order (fields: string[], direction?: Direction): TermBase;
         limit (size: Number): TermBase;
@@ -20,24 +24,26 @@ declare namespace hz {
         below (spec: any, bound?: Bound): TermBase;
     }
 
-    type WriteOp = Object | Object[];
-
     interface Collection extends TermBase {
         store (docs: WriteOp): Observable<any>;
         upsert (docs: WriteOp): Observable<any>;
         insert (docs: WriteOp): Observable<any>;
         replace (docs: WriteOp): Observable<any>;
         update (docs: WriteOp): Observable<any>;
+
         remove (docs: IdValue): Observable<any>;
-        remove (docs: IdValue[]): Observable<any>;
+        removeAll (docs: IdValue[]): Observable<any>;
     }
 
-    interface User extends TermBase {}
+    interface User extends Feed {}
 
     interface HorizonInstance {
         (name: string): Collection;
 
         currentUser (): User;
+
+        hasAuthToken (): boolean;
+        authEndpoint (name: string): Observable<string>;
 
         aggregate (aggs: any): TermBase;
         model (fn: Function): TermBase;
@@ -69,6 +75,12 @@ declare namespace hz {
         clearAuthTokens (): void;
     }
 }
+
+export type HorizonOptions = hz.HorizonOptions;
+export type HorizonInstance = hz.HorizonInstance;
+export type TermBase = hz.TermBase;
+export type Collection = hz.Collection;
+export type User = hz.User;
 
 declare var Horizon: hz.Horizon;
 export default Horizon;
