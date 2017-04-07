f0e12064f34f95013012067450b8468a6e0ec71b
Ruben Biesemans <ruben.biesemans@spikes.be>
2017-04-07 11:33:52 +0200

    Acquire-TokenReq

--- server/src/auth/adal.js
+++ server/src/auth/adal.js
@@ -1,1 +1,53 @@
-//Adal auth implementation
+'use strict';
+
+const logger = require('../logger');
+const auth_utils = require('./utils');
+
+const https = require('https');
+const Joi = require('joi');
+const querystring = require('querystring');
+const url = require('url');
+
+const options_schema = Joi.object().keys({
+  path: Joi.string().required(),
+  id: Joi.string().required(),
+  secret: Joi.string().required(),
+}).unknown(false);
+
+function google(horizon, raw_options) {
+  const options = Joi.attempt(raw_options, options_schema);
+  const client_id = options.id;
+  const client_secret = options.secret;
+  const provider = options.path;
+
+  const oauth_options = { horizon, provider };
+
+  oauth_options.make_acquire_url = (state, redirect_uri) =>
+    url.format({ protocol: 'https',
+                 host: 'login.microsoftonline.com',
+                 pathname: '/SPIKES1BE.onmicrosoft.com/oauth2/authorize',
+                 query: { client_id, redirect_uri, state} });
+
+  oauth_options.make_token_request = (code, redirect_uri) => {
+    const req = https.request({ method: 'POST',
+                                host: 'login.microsoftonline.com',
+                                path: '/SPIKES1BE.onmicrosoft.com/oauth2/access_token',
+                                headers: { accept: 'application/json' } });
+
+    req.write(querystring.stringify({ code, client_id, client_secret, redirect_uri }));
+
+    return req;
+  };
+
+  oauth_options.make_inspect_request = (access_token) => {
+    logger.debug(`using access token: ${access_token}`);
+    const path = `/oauth2/v1/userinfo?${querystring.stringify({ access_token })}`;
+    return https.request({ host: 'www.googleapis.com', path });
+  };
+
+  oauth_options.extract_id = (user_info) => user_info && user_info.id;
+
+  auth_utils.oauth2(oauth_options);
+}
+
+module.exports = google;
